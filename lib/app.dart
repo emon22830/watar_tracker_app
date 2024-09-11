6b133757bf006cbb1c04cb3bfba148c2265ef1e0
emon22830 <mdemon22830@gmail.com>
2024-09-11 12:21:55 +0800

    Initial commit

---
+++ lib/app.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:watar_tracker_app/homescreen.dart';
+
+class WaterTrackerApp extends StatelessWidget {
+  const WaterTrackerApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:watar_tracker_app/app.dart';
+
+void main(){
+  runApp(const WaterTrackerApp());
+}
