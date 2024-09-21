831394e8ed5f084acacad928952d9b1da4854754
emon22830 <mdemon22830@gmail.com>
2024-09-21 15:44:01 +0800

    Updated1

--- lib/homescreen.dart
+++ lib/homescreen.dart
@@ -8,14 +8,102 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+
+  final TextEditingController _glassNoTEController = TextEditingController(
+      text: "2"
+  );
+
+  List<WaterTrack> waterTrackList = [];
+
+  void _addNewWaterTrack() {
+    if (_glassNoTEController.text.isEmpty) {
+      _glassNoTEController.text = '1';
+    }
+    final int nofGlassed = int.tryParse(_glassNoTEController.text) ?? 1;
+    WaterTrack waterTrack = WaterTrack(
+      noOfGlasses: noOfGlasses,
+      dateTime: DateTime.now(),
+    );
+    waterTrackList.add(waterTrack);
+  }
+
+  int getTotalGlassCount(){
+    for(WaterTrack t in waterTrackList){
+      counter += 0;
+    }
+    return counter;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar:AppBar(
+      appBar: AppBar(
         backgroundColor: Colors.deepPurple,
         title: const Text('Water Tracker'),
+        centerTitle: true,
       ),
-
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Text(
+            getTotalGlassCount().toString(),
+            style:const  TextStyle(fontSize: 24, fontWeight: FontWeight.w600),
+          ),
+          Text(
+            'Glass/s',
+            style: TextStyle(fontSize: 16),
+          ),
+          const SizedBox(
+            height: 16,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SizedBox(
+                width: 50,
+                child: TextField(
+                  controller: _glassNoTEController,
+                  keyboardType: TextInputType.number,
+                  textAlign: TextAlign.center,
+                ),
+              ),
+              TextButton(
+                onPressed: _addNewWaterTrack,
+                child: const Text('Add'),
+              ),
+            ],
+          ),
+          const SizedBox(
+            height: 24,
+          ),
+          Expanded(
+            child: ListView.separated(
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    title: Text('Time'),
+                    subtitle: Text('Date'),
+                    leading: Text('1'),
+                    trailing: IconButton(
+                      onPressed: () {},
+                      icon: Icon(Icons.delete),
+                    ),
+                  );
+                },
+                separatorBuilder: (context, index) {
+                  return Divider();
+                },
+                itemCount: 3),)
+        ],
+      ),
     );
+  }
+}
+
+class WaterTrack {
+  final int noOfGlasses;
+  final DateTime dateTime;
+
+  WaterTrack({required this.noOfGlasses, required this.dateTime}) {
+
   }
 }
